d90ebf66aa574a036a0960ab65342368dc4c2e72
Pearce <Bobtarznit@gmail.com>
2013-03-17 13:42:35 -0500

    Finished Exercise 163
    Added mail sorting for names

--- exercise 163.rkt
+++ exercise 163.rkt
@@ -15,28 +15,27 @@
 ; interp. a List-of-Mail is a list of the Mail
 
 ; Mail LOM -> LOM
-; insert a Mail into the sorted LOM
-(check-expect (insert (make-mail "Bob" 2 "Hello, my name is Bob!") empty)
+; insert a Mail into the sorted LOM by date
+(check-expect (insert-date (make-mail "Bob" 2 "Hello, my name is Bob!") empty)
               (list (make-mail "Bob" 2 "Hello, my name is Bob!")))
-(check-expect (insert (make-mail "Bob" 2 "Hello, my name is Bob!")
+(check-expect (insert-date (make-mail "Bob" 2 "Hello, my name is Bob!")
                       (list (make-mail "Robert" 1 "Kon nishiwa")
                             (make-mail "Bobby" 5 "Hola, Anonymous!")))
               (list (make-mail "Robert" 1 "Kon nishiwa")
                      (make-mail "Bob" 2 "Hello, my name is Bob!")
                      (make-mail "Bobby" 5 "Hola, Anonymous!")))
 
-(define (insert Mail LOM)
+(define (insert-date Mail LOM)
   (cond
     [(empty? LOM) (list Mail)]
     [else (if (<= (mail-date Mail) (mail-date (first LOM)))
               (cons Mail LOM)
-              (cons (first LOM) (insert Mail (rest LOM))))]))
-     
+              (cons (first LOM) (insert-date Mail (rest LOM))))]))  
      
 ; LOM -> LOM
-; produces a sorted version of LOM in ascending order
-(check-expect (sort->mail empty) empty)
-(check-expect (sort->mail
+; produces a sorted version of LOM by date in ascending order
+(check-expect (sort->mail_date empty) empty)
+(check-expect (sort->mail_date
                (list (make-mail "Bob" 2 "Hello, my name is Bob!")
                      (make-mail "Bobby" 5 "Hola, Anonymous!")
                      (make-mail "Robert" 1 "Kon nishiwa")))
@@ -44,8 +43,43 @@
                      (make-mail "Bob" 2 "Hello, my name is Bob!")
                      (make-mail "Bobby" 5 "Hola, Anonymous!")))
 
-(define (sort->mail LOM)
+(define (sort->mail_date LOM)
   (cond
     [(empty? LOM) empty]
     [else
-     (insert (first LOM) (sort->mail (rest LOM)))]))
+     (insert-date (first LOM) (sort->mail_date (rest LOM)))]))
+
+; Mail LOM -> LOM
+; insert a Mail into the sorted LOM by name
+(check-expect (insert-name (make-mail "Bob" 2 "Hello, my name is Bob!") empty)
+              (list (make-mail "Bob" 2 "Hello, my name is Bob!")))
+(check-expect (insert-name (make-mail "Bob" 2 "Hello, my name is Bob!")
+                      (list (make-mail "Bobby" 5 "Hola, Anonymous!")
+                            (make-mail "Robert" 1 "Kon nishiwa")))
+              (list (make-mail "Bob" 2 "Hello, my name is Bob!")
+                    (make-mail "Bobby" 5 "Hola, Anonymous!")
+                    (make-mail "Robert" 1 "Kon nishiwa")))
+
+(define (insert-name Mail LOM)
+  (cond
+    [(empty? LOM) (list Mail)]
+    [else (if (string<? (mail-from Mail) (mail-from (first LOM)))
+              (cons Mail LOM)
+              (cons (first LOM) (insert-name Mail (rest LOM))))]))
+
+; LOM -> LOM
+; produces a sorted version of LOM by name in alphabetical order
+(check-expect (sort->mail_name empty) empty)
+(check-expect (sort->mail_name
+               (list (make-mail "Bob" 2 "Hello, my name is Bob!")
+                     (make-mail "Bobby" 5 "Hola, Anonymous!")
+                     (make-mail "Robert" 1 "Kon nishiwa")))
+               (list (make-mail "Bob" 2 "Hello, my name is Bob!")
+                     (make-mail "Bobby" 5 "Hola, Anonymous!")
+                     (make-mail "Robert" 1 "Kon nishiwa")))
+
+(define (sort->mail_name LOM)
+  (cond
+    [(empty? LOM) empty]
+    [else
+     (insert-name (first LOM) (sort->mail_name (rest LOM)))]))
